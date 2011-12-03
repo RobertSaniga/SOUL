ebc61de707ba6c282c594e834e77135d7d296f17
Robert Saniga <san059@vsb.cz>
2011-12-03 17:18:00 +0100

    initial commit

---
+++ src/main/java/cz/saniga/android/diploma/soul/model/AbstractQuestion.java
@@ -0,0 +1,5 @@
+package cz.saniga.android.diploma.soul.model;
+
+public class AbstractQuestion {
+	private int id;
+}
---
+++ src/main/java/cz/saniga/android/diploma/soul/model/mmpi2/MMPI2Question.java
@@ -0,0 +1,10 @@
+package cz.saniga.android.diploma.soul.model.mmpi2;
+
+import java.util.List;
+
+import cz.saniga.android.diploma.soul.model.AbstractQuestion;
+
+public class MMPI2Question extends AbstractQuestion {
+	
+	private List<String> texts;
+}
