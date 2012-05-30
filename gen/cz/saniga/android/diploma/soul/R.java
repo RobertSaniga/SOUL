4725303904fb4b29c21e6cc4ab2b4529cbda4264
Robert Saniga <san059@vsb.cz>
2012-05-30 23:41:08 +0200

    Added dimensions resource file for tablets

--- gen/cz/saniga/android/diploma/soul/R.java
+++ gen/cz/saniga/android/diploma/soul/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class dimen {
         public static final int choiceHorizontalRadioSidePadding=0x7f050000;
+        public static final int choiceHorizontalRadioWidth=0x7f050001;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
